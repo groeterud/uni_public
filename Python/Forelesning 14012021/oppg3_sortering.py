5d65d54670331af11a92f9c0df231c178d3f1668
groeterud <groeterud@users.noreply.github.com>
2021-01-15 16:10:09 +0100

    småtterier

--- Python/Forelesning 14012021/oppg3_sortering.py
+++ Python/Forelesning 14012021/oppg3_sortering.py
@@ -2,7 +2,6 @@
 #usortert=[40.3, 55.0, 45.7, 43.3, 50.3, 45.9, 53.5, 43.0, 44.2, 44.0, 47.4, 44.0, 33.6, 55.1, 48.8, 50.4, 37.8, 60.3, 46.6]
 usortert=list(range(750,0,-1))
 
-
 print('Lista før sortering er:      ',usortert)
 print()
 print()

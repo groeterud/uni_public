192ac1591cef3116442d14de9caee210fb036fcc
groeterud <groeterud@users.noreply.github.com>
2021-01-28 15:58:16 +0100

    Fjerne where på gruppe

--- SQL/Forelesning28012021/Script1_MengdeGruppering.sql
+++ SQL/Forelesning28012021/Script1_MengdeGruppering.sql
@@ -15,13 +15,11 @@ WHERE Stillingskode IS NOT NULL;
 -- hvor mange ansatte pr stillingskode
 SELECT COUNT(*) AS Antall, Stillingskode
 FROM Ansatt
-WHERE Stillingskode IS NOT NULL
 GROUP BY Stillingskode;
 
 -- det høyeste og laveste lønnstrinnet innenfor hver stillingskode. 
 SELECT Stillingskode, MIN(Lønnstrinn) AS LavesteLønn, MAX(Lønnstrinn) AS HøyesteLønn
 FROM Ansatt
-WHERE Stillingskode IS NOT NULL
 GROUP BY Stillingskode;
 
 

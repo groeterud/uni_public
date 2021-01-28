7ac8f7a3785d782c823fa21bfaf2400f27ee8f2c
groeterud <groeterud@users.noreply.github.com>
2021-01-28 14:43:45 +0100

    Lager ny / rename

---
+++ SQL/Forelesning28012021/Script1_MengdeGruppering.sql
@@ -0,0 +1,44 @@
+-- Gruppering
+USE gruppering;
+SELECT * 
+FROM Ansatt;
+-- hvor mange ansatte
+SELECT COUNT(*) AS AntallAnsatte
+FROM Ansatt
+WHERE Ansattnr IS NOT NULL;
+
+-- hvor mange forskjellige stillingskoder
+SELECT COUNT(Stillingskode) AS AntallStillingskoder
+FROM Ansatt
+WHERE Stillingskode IS NOT NULL;
+
+-- hvor mange ansatte pr stillingskode
+SELECT COUNT(*) AS Antall, Stillingskode
+FROM Ansatt
+WHERE Stillingskode IS NOT NULL
+GROUP BY Stillingskode;
+
+-- det høyeste og laveste lønnstrinnet innenfor hver stillingskode. 
+SELECT Stillingskode, MIN(Lønnstrinn) AS LavesteLønn, MAX(Lønnstrinn) AS HøyesteLønn
+FROM Ansatt
+WHERE Stillingskode IS NOT NULL
+GROUP BY Stillingskode;
+
+
+-- Dobbelgruppering, viser alle unike kombinasjoner. 
+SELECT Stillingskode, Lønnstrinn
+FROM Ansatt
+GROUP BY Stillingskode, Lønnstrin;
+
+
+-- noe som lar oss telle antall treff i de unike kombinasjonene: 
+-- hvor mange ansatte pr stillingskode
+SELECT COUNT(*) AS Antall, Stillingskode, Lønnstrinn
+FROM Ansatt
+GROUP BY Stillingskode, Lønnstrinn;
+
+
+-- Oppsummering
+-- Gruppekriteriet: i SELECT-delen og GROUP BY
+-- Gruppebetingelse: HAVING
+-- COUNT(*) vs COUNT(kolonnenavn)

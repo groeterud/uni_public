214481b8b28121e48107eb89dbe624930180cfc9
groeterud <groeterud@users.noreply.github.com>
2021-02-08 16:50:58 +0100

    Update Etterarbeid-oppg3.py

--- Python/Forelesning 080221/Etterarbeid-oppg3.py
+++ Python/Forelesning 080221/Etterarbeid-oppg3.py
@@ -1,3 +1,15 @@
+def bubble_sort2d_list(liste,cond):
+    i=0
+    while i !=len(liste):
+        for n in range(0,(len(liste)-1)-i):  
+            if liste[n][cond]>=liste[n+1][cond]:
+                storste_tall=liste[n]
+                liste[n]=liste[n+1]
+                liste[n+1]=storste_tall
+        i+=1
+    
+    return(liste)
+
 vare=[]
 print(vare)
 
@@ -14,18 +26,8 @@ while vnr!='':
 
 varefil.close()
 
-def bubble_sort2d_list(liste,cond):
-    i=0
-    while i !=len(liste):
-        for n in range(0,(len(liste)-1)-i):  
-            if liste[n][cond]>=liste[n+1][cond]:
-                storste_tall=liste[n]
-                liste[n]=liste[n+1]
-                liste[n+1]=storste_tall
-        i+=1
-    
-    return(liste)
 
+#første parameter = listevariabel, andre parameter, kolonneverdi som skal sorteres etter. 
 vare=bubble_sort2d_list(vare,1)
 print(vare)
 

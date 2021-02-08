f2c4d7a8a4a03cbaf43952a752d30e6e408a1b2d
groeterud <groeterud@users.noreply.github.com>
2021-02-08 10:36:49 +0100

    more stuff from lect

--- Python/Forelesning 080221/script-2-dict.py
+++ Python/Forelesning 080221/script-2-dict.py
@@ -30,10 +30,47 @@ try:
 except KeyError:
     print('Nøkkelen finnes ikke my dude')
 '''
-
 #søkekriteriet som inndata
+'''
 navn=input('Oppgi navn på kontakten ')
 if navn in kontakter:
     print(navn,'har telefonnr',kontakter[navn])
 else:
     print('Kontakten',navn,'finnes ikke')
+'''
+
+#print av alle nøkler med verdier i en for-løkke
+for key in kontakter:
+    print(key,kontakter[key])
+print()
+
+
+#
+#legge til kontakt
+'''
+navn=input('Oppgi navn: ')
+tlfnr=int(input('Oppgi telefonnummer: '))
+
+kontakter[navn]=tlfnr
+
+print('kvittering')
+print(navn,'sitt tlf nr er',kontakter[navn])
+print()
+print('Hele lista:')
+print(kontakter)
+print()
+#dersom key-value finnes fra før vil dete bli en overskriving/nytt tlfnr
+'''
+
+#slette kontakt
+'''
+navn=input('Oppgi navn på kontakten som skal slette: ')
+if navn in kontakter:
+    print('funnet')
+    del kontakter[navn]
+    print(kontakter)
+else:
+    print('ikke funnet')
+'''
+
+

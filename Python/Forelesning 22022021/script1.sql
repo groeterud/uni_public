f9c422129008ef9cfb8dd4e34f234c0af73bc1e6
groeterud <groeterud@users.noreply.github.com>
2021-02-23 02:08:12 +0100

    oppdatering av varer
    Lagt til oppdatering av varer - L153
    
    Måtte endre litt på originale inndata slik at vi tok vare på de bedre... Kan være verdt å evaluere hvorvidt det ikke hadde vært bedre å gjøre en spørring istedenfor å holde hele databasen i minnet.

--- Python/Forelesning 22022021/script1.sql
+++ Python/Forelesning 22022021/script1.sql
@@ -8,3 +8,5 @@ CREATE USER 'Lagersjefen2021' IDENTIFIED BY 'lagerpw';
 GRANT SELECT ON Vare TO 'Lagersjefen2021';
 GRANT INSERT ON Vare TO 'Lagersjefen2021';
 GRANT DELETE ON Vare TO 'Lagersjefen2021';
+
+GRANT UPDATE ON Vare TO 'Lagersjefen2021';
--- Python/Forelesning 22022021/vis_leggetil_slette.py
+++ Python/Forelesning 22022021/vis_leggetil_slette.py
@@ -53,7 +53,7 @@ def slett_valgt_vare():
         del_markor.execute("DELETE FROM Vare WHERE Betegnelse=%s",(valgt,))
         mindatabase.commit()
         update_listbox()
-        
+
     del_markor.close()
 
 
@@ -88,13 +88,16 @@ def legg_til_vindu():
             settinn_markor.execute(sett_inn_vare,data_ny_vare)
             mindatabase.commit()
             alt_gikk_bra(data_ny_vare)
+            legg_til_window.destroy()
         except:
             noe_gikk_galt(data_ny_vare)
+            legg_til_window.destroy()
 
         settinn_markor.close()    
 
+
     legg_til_window=Toplevel()
-    legg_til_window.title('Oppdater database')
+    legg_til_window.title('Legg til ny vare')
 
 
     lbl_vnr_top=Label(legg_til_window,text='Varenr:')
@@ -147,6 +150,110 @@ def legg_til_vindu():
     btn_lagre=Button(legg_til_window,text='Lagre',width=20,command=lagre)
     btn_lagre.grid(row=6,column=0,columnspan=2,padx=5,pady=(15,5),sticky=E)
 
+
+def oppdater_vare():
+    valgt=lst_varer.get(lst_varer.curselection())
+    valgt=str(valgt)
+    def oppdater():
+        #oppretter cursoren / markøren
+        settinn_markor=mindatabase.cursor()
+        
+        #bruke databasen
+        varenrn=vnr_updt_sv.get()
+        varenavn=betegnelse_updt_sv.get()
+        pris=float(pris_updt_sv.get())
+        katNr=int(katnr_updt_sv.get())
+        antall=int(antall_updt_sv.get())
+        hylle=hylle_updt_sv.get()
+
+        oppdater_vare=("UPDATE VARE SET VNr=%s,Betegnelse=%s,Pris=%s,KatNr=%s,Antall=%s,Hylle=%s WHERE Betegnelse=%s")
+        data_ny_vare = (varenrn,varenavn,pris,katNr,antall,hylle,valgt)
+
+        try:
+            settinn_markor.execute(oppdater_vare,data_ny_vare)
+            mindatabase.commit()
+            messagebox.showinfo('Vellykket','Dette gikk strålende')
+            oppdater_window.destroy()
+            update_listbox()
+        except:
+            messagebox.showerror('Feil','Kunne ikke lagre dataene, prøv igjen')
+            oppdater_window.destroy()
+
+        settinn_markor.close()   
+
+    def hent_eksisterende():
+        for x in range(0,len(varer)):
+            if varer[x]==valgt:
+                vnr_updt_sv.set(tabell[x][0])
+                betegnelse_updt_sv.set(tabell[x][1])
+                pris_updt_sv.set(tabell[x][2])
+                katnr_updt_sv.set(tabell[x][3])
+                antall_updt_sv.set(tabell[x][4])
+                hylle_updt_sv.set(tabell[x][5])
+    
+
+
+    oppdater_window=Toplevel()
+    oppdater_window.title('Oppdater vare')
+
+
+    lbl_vnr_updt=Label(oppdater_window,text='Varenr:')
+    lbl_vnr_updt.grid(row=0,column=0,padx=5,pady=5,sticky=E)
+
+    vnr_updt_sv=StringVar()
+    ent_vnr_updt=Entry(oppdater_window,width=5,textvariable=vnr_updt_sv)
+    ent_vnr_updt.grid(row=0,column=1,padx=5,pady=5,sticky=W)
+
+
+    lbl_bet_updt=Label(oppdater_window,text='Betegnelse:')
+    lbl_bet_updt.grid(row=1,column=0,padx=5,pady=5,sticky=E)
+
+    betegnelse_updt_sv=StringVar()
+    ent_bet_updt=Entry(oppdater_window,width=30,textvariable=betegnelse_updt_sv)
+    ent_bet_updt.grid(row=1,column=1,padx=5,pady=5,sticky=W)
+
+
+    lbl_pris_updt=Label(oppdater_window,text='Pris:')
+    lbl_pris_updt.grid(row=2,column=0,padx=5,pady=5,sticky=E)
+
+    pris_updt_sv=StringVar()
+    ent_pris_updt=Entry(oppdater_window,width=11,textvariable=pris_updt_sv)
+    ent_pris_updt.grid(row=2,column=1,padx=5,pady=5,sticky=W)
+
+
+    lbl_katnr_updt=Label(oppdater_window,text='Kategori #')
+    lbl_katnr_updt.grid(row=3,column=0,padx=5,pady=5,sticky=E)
+
+    katnr_updt_sv=StringVar()
+    ent_katnr_updt=Entry(oppdater_window,width=2,textvariable=katnr_updt_sv)
+    ent_katnr_updt.grid(row=3,column=1,padx=5,pady=5,sticky=W)
+
+
+    lbl_antall_updt=Label(oppdater_window,text='Antall:')
+    lbl_antall_updt.grid(row=4,column=0,padx=5,pady=5,sticky=E)
+
+    antall_updt_sv=StringVar()
+    ent_antall_updt=Entry(oppdater_window,width=5,textvariable=antall_updt_sv)
+    ent_antall_updt.grid(row=4,column=1,padx=5,pady=5,sticky=W)
+
+
+    lbl_hylle=Label(oppdater_window,text='Hylleplassering:')
+    lbl_hylle.grid(row=5,column=0,padx=5,pady=5,sticky=E)
+
+    hylle_updt_sv=StringVar()
+    ent_hylle=Entry(oppdater_window,width=3,textvariable=hylle_updt_sv)
+    ent_hylle.grid(row=5,column=1,padx=5,pady=5,sticky=W)
+
+    #btn_hent=Button(oppdater_window,text='Hent verdier',width=20,command=hent_eksisterende)
+    #btn_hent.grid(row=6,column=0,columnspan=2,padx=5,pady=(15,5),sticky=E)
+
+    btn_lagre=Button(oppdater_window,text='Lagre',width=20,command=oppdater)
+    btn_lagre.grid(row=7,column=0,columnspan=2,padx=5,pady=(15,5),sticky=E)
+
+    hent_eksisterende()
+
+    
+
 #koble opp til db
 mindatabase=mysql.connector.connect(host='localhost',
                                     port=3306,
@@ -159,15 +266,15 @@ vare_markor=mindatabase.cursor()
 
 #3. Bruke databasen
 vare_markor.execute('SELECT * FROM Vare')
-
+tabell=vare_markor.fetchall()
 #4. Bruke resultatet.
 varer=[]
-for row in vare_markor:
+for row in tabell:
     varer+=[row[1]]
 
 #GUI
 window=Tk()
-window.title('Varer')
+window.title('LagerMaster v0.5')
 
 y_scroll=Scrollbar(window,orient=VERTICAL)
 y_scroll.grid(row=0,column=2,rowspan=10,padx=(0,100),pady=5,sticky=NS)
@@ -199,9 +306,12 @@ lst_varer.bind('<<ListboxSelect>>',hent_pris_lager)
 btn_slett=Button(window,text='Slett valgt vare',width=20,command=slett_valgt_vare)
 btn_slett.grid(row=2,column=3,columnspan=2,padx=5,pady=5,sticky=E)
 
+#TBA
+btn_oppdater=Button(window,text='Oppdater valgt vare',width=20,command=oppdater_vare)
+btn_oppdater.grid(row=3,column=3,columnspan=2,padx=5,pady=5,sticky=E)
+
 btn_lagre=Button(window,text='Legg til ny vare',width=20,command=legg_til_vindu)
 btn_lagre.grid(row=8,column=3,columnspan=2,padx=5,pady=(15,5),sticky=E)
-
 
 
 window.mainloop()

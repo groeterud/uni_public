a990703046b5dbe26a2e22eff412b8ac52baec32
groeterud <groeterud@users.noreply.github.com>
2021-05-21 15:43:48 +0200

    yussss

--- Python/EksamensprepV2021/V2020/spm6.py
+++ Python/EksamensprepV2021/V2020/spm6.py
@@ -2,9 +2,21 @@ from BilDeling import Bil
 from lesTextFil import LesTextFil
 import pickle
 
-fil=LesTextFil('Bil.txt',5)
+#fil=LesTextFil('Bil.txt',numrows=5)
+filer=[]
+for x in range(0,1):
+    if x ==0:
+        fil=LesTextFil('Bil.txt',5)
+        filer+=[fil]
+    elif x==1:
+        fil=LesTextFil('Kunde.txt',4)
+        filer+=[fil]
 
-biler=fil.file_to_2d_list()
+for fil in filer:
+    print(fil.file_to_2d_list())
+
+
+biler=filer[0].file_to_2d_list()
 
 output=open('biler.dat','wb')
 
@@ -13,3 +25,4 @@ for x in range(len(biler)):
     print('Dumped:\t',biler[x])
 
 output.close()
+

2ceebb61012093094ff1aa5a75976ac4f3a677ff
groeterud <groeterud@users.noreply.github.com>
2021-08-29 12:17:46 +0200

    Update test.py

--- Python/Eksamensprep/test.py
+++ Python/Eksamensprep/test.py
@@ -1,5 +1,24 @@
-tall = input('Skriv inn tall: ')
-sum=0
-for x in range(0,len(tall)):
-    sum+=tall[x]
-print(sum)
+def find_uniq(arr):
+    # your code here
+    
+    first=[]
+    second=[]
+    
+    for x in range(len(arr)):
+        if x==0:
+            first+=[arr[x]]
+        else:
+            if arr[x]==first[0]:
+                first+=[arr[x]]
+            else:
+                second+=[arr[x]]
+        
+    
+    if len(first)>len(second):
+        return second[0]
+    else:
+        return first[0]
+        
+
+find_uniq([1, 1, 1, 2, 1, 1])
+    

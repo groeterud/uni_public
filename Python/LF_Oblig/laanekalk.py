a0737baca671aa3f42e1a941091a251796577ab8
groeterud <groeterud@users.noreply.github.com>
2021-10-06 15:10:19 +0200

    lånekalender

--- Python/LF_Oblig/laanekalk.py
+++ Python/LF_Oblig/laanekalk.py
@@ -1,22 +1,48 @@
 class LaaneKalkulator:
-    def __init__(self,egenkap,inntekt) -> None:
+    def __init__(self,egenkap,inntekt,kjopesum) -> None:
         self.__egenkap=egenkap
         self.__inntekt=inntekt
+        self.__kjopesum=kjopesum
+        self.__laanesum=kjopesum-egenkap
     
     def get_egenkap(self):
         return self.__egenkap
     def get_inntekt(self):
         return self.__inntekt
+    def get_kjopesum(self):
+        return self.__kjopesum
+    def set_kjopesum(self,laanesum):
+        self.__kjopesum=laanesum
+        
     def set_egenkap(self,egenkap):
         self.__egenkap=egenkap
     def set_inntekt(self,inntekt):
         self.__inntekt=inntekt
+
+    def get_laanesum(self):
+        return self.__laanesum
     
     def inntekt_limit(self) -> bool:
-        if self.__inntekt<self.__inntekt*5:
-            return False
-        return True
+        if self.__laanesum<self.__inntekt*5:
+            return True
+        return False
+
+    def check_egenkap(self):
+        if self.__egenkap>=self.__laanesum*0.15:
+            return True
+        return False
+
 
     def eval(self) -> bool:
-        if inntekt_limit():
-            pass
+        if self.inntekt_limit():
+            if self.check_egenkap():
+                return True
+        return False
+
+    def __str__(self) -> str:
+        return f"""
+        Lånesum: {self.__laanesum}\n
+        Egenkapital: {self.__egenkap}\n
+        Inntekt: {self.__inntekt}\n
+        Kjøpesum: {self.__kjopesum}
+        """
---
+++ Python/LF_Oblig/program.py
@@ -0,0 +1,6 @@
+from laanekalk import LaaneKalkulator
+
+kalk_en = LaaneKalkulator(75,100,500)
+
+if kalk_en.eval():
+    print(kalk_en)
